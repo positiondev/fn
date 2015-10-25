e595cc1d2072a2e44887bb0d672ad5fbc5c1f409
Daniel Patterson <dbp@dbpmail.net>
2015-10-24 20:41:44 -0400

    Add redis to example application.

--- example/Main.hs
+++ example/Main.hs
@@ -13,7 +13,9 @@ import           Data.Monoid
 import           Data.Pool
 import           Data.Text                  (Text)
 import qualified Data.Text                  as T
+import qualified Data.Text.Encoding         as T
 import qualified Database.PostgreSQL.Simple as PG
+import qualified Database.Redis             as R
 import           Heist
 import           Heist.Interpreted
 import           Network.HTTP.Types
@@ -25,6 +27,7 @@ import           Web.Fn
 data Ctxt = Ctxt { _req   :: Request
                  , _heist :: HeistState IO
                  , _db    :: Pool PG.Connection
+                 , _redis :: R.Connection
                  }
 
 makeLenses ''Ctxt
@@ -47,7 +50,8 @@ initializer =
                                                           "111"
                                                           "fn_db"))
                               PG.close 1 60 20
-         return (Ctxt defaultRequest hs' pgpool)
+         rconn <- R.connect R.defaultConnectInfo
+         return (Ctxt defaultRequest hs' pgpool rconn)
 
 main :: IO ()
 main = withStdoutLogging $
@@ -67,6 +71,7 @@ app ctxt =
              ,path "template" ==> templateHandler
              ,path "db" /? param "number" ==> dbHandler
              ,path "segment" // segment ==> segmentHandler
+             ,path "redis" // segment /? paramOptional "set" ==> redisHandler
              ]
     `fallthrough` return (responseLBS status404 [] "Page not found.")
 
@@ -74,7 +79,8 @@ indexHandler :: Ctxt -> IO (Maybe Response)
 indexHandler _ =
   Just <$>
     W.text status200 []
-           "Try /param?id=123, /template, /db?number=123, or /segment/foo"
+           ("Try /param?id=123, /template, /db?number=123, /segment/foo,"
+           <> " or /redis/key or /redis/key?set=new")
 
 paramHandler :: Ctxt -> Int -> IO (Maybe Response)
 paramHandler _ i =
@@ -97,3 +103,18 @@ dbHandler ctxt n =
 
 segmentHandler :: Ctxt -> Text -> IO (Maybe Response)
 segmentHandler _ seg = Just <$> W.text status200 [] seg
+
+redisHandler :: Ctxt -> Text -> Either Text Text -> IO (Maybe Response)
+redisHandler ctxt key new =
+  do res <- R.runRedis (ctxt ^. redis) $
+              do let k = T.encodeUtf8 key
+                 case new of
+                   Left _ -> R.get k
+                   Right new' -> R.getset k (T.encodeUtf8 new')
+     Just <$> case res of
+                Left err ->
+                  W.text status500 []
+                         (T.pack (show err))
+                Right value ->
+                  W.text status200 []
+                         (T.pack (show value))

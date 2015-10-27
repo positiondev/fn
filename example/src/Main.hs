0eaf232c2e062df6dc51d0ffeb2edf2ee7cf5802
Daniel Patterson <dbp@dbpmail.net>
2015-10-26 23:10:35 -0400

    example: get port from ENV; default to 8000.

--- example/src/Main.hs
+++ example/src/Main.hs
@@ -3,15 +3,19 @@
 
 import           Control.Exception        (SomeException (..), catch)
 import           Control.Logging
+import           Data.Monoid
+import qualified Data.Text                as T
 import           Network.Wai.Handler.Warp
+import           System.Environment
 
 import           Site
 
 main :: IO ()
 main = withStdoutLogging $
-       do log' "Starting server on port 8000..."
+       do port <- maybe 8000 read <$> lookupEnv "PORT"
+          log' $ "Starting server on port " <> T.pack (show port) <> "..."
           (app', shutdown) <- app
-          catch (run 8000 app')
+          catch (run port app')
                 (\(_ :: SomeException) ->
                    do log' "Shutting down..."
                       shutdown)

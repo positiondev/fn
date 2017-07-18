c060839d37c26f092ee7d7203730f677d43cf5b8
Libby Horacek <libby@daydrea.me>
2017-07-17 22:09:24 -0400

    Clean up hlint warnings in Digestive

--- fn-extra/src/Web/Fn/Extra/Digestive.hs
+++ fn-extra/src/Web/Fn/Extra/Digestive.hs
@@ -10,24 +10,20 @@ import           Control.Monad.Trans.Resource
 import           Data.ByteString              (ByteString)
 import           Data.Maybe                   (fromMaybe)
 import           Data.Text                    (Text)
-import qualified Data.Text                    as T
 import qualified Data.Text.Encoding           as T
 import           Network.HTTP.Types.Method
 import           Network.Wai                  (Request (..))
 import           Network.Wai.Parse            (File, FileInfo (..), fileContent,
-                                               parseRequestBody,
-                                               tempFileBackEndOpts)
-import           System.Directory             (getTemporaryDirectory)
-import           Text.Digestive
-import           Text.Digestive.Types
-import           Web.Fn                       hiding (File, fileContent)
+                                               parseRequestBody)
+import           Text.Digestive               (Form, View, FormInput(..), Env, fromPath, postForm, getForm)
+import           Web.Fn                       hiding (File)
 
 queryFormEnv :: [(ByteString, Maybe ByteString)] -> [File FilePath] -> Env IO
 queryFormEnv qs fs = \pth ->
-  let qs' = map TextInput $ map (T.decodeUtf8 . fromMaybe "" . snd) $ filter (forSubForm pth) qs
-      fs' = map FileInput $ map (fileContent . snd) $ filter (forSubForm pth) $ filter fileNameNotEmpty fs
+  let qs' = map (TextInput . T.decodeUtf8 . fromMaybe "" . snd) $ filter (forSubForm pth) qs
+      fs' = map (FileInput . fileContent . snd) $ filter (forSubForm pth) $ filter fileNameNotEmpty fs
   in return $ qs' ++ fs'
-  where fileNameNotEmpty (formName, fileInfo) = Network.Wai.Parse.fileName fileInfo /= "\"\""
+  where fileNameNotEmpty (_formName, fileInfo) = Network.Wai.Parse.fileName fileInfo /= "\"\""
         forSubForm pth = (==) (fromPath pth) . T.decodeUtf8 . fst
 
 requestFormEnv :: FnRequest -> ResourceT IO (Env IO)
@@ -41,7 +37,7 @@ requestFormEnv req = do
        Nothing -> liftIO $ parseRequestBody (tempFileBackEnd' st)
                                             (fst req)
        Just (q,_) -> return q
-  return $ queryFormEnv ((map (second Just) query) ++ queryString (fst req)) files
+  return $ queryFormEnv (map (second Just) query ++ queryString (fst req)) files
 
 -- | This function runs a form and passes the function in it's last
 -- argument the result, which is a 'View' and an optional result. If
@@ -58,7 +54,7 @@ runForm ctxt nm frm k =
   runResourceT $ let r = fst (getRequest ctxt) in
     if requestMethod r == methodPost
     then do env <- requestFormEnv (getRequest ctxt)
-            r <- liftIO $ postForm nm frm (const (return env))
-            liftIO $ k r
-    else do r <- (,Nothing) <$> liftIO (getForm nm frm)
-            liftIO $ k r
+            r' <- liftIO $ postForm nm frm (const (return env))
+            liftIO $ k r'
+    else do r' <- (,Nothing) <$> liftIO (getForm nm frm)
+            liftIO $ k r'
